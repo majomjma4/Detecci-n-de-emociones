f93c8b6512e5be804c9543054b0b795724f2ed27
Maria José <monterosmarijo2004@gmail.com>
2025-12-14 21:51:18 -0500

    Creación archivo modelos.py

---
+++ face_detector/modelos.py
@@ -0,0 +1,26 @@
+# face_detector/modelos.py
+"""
+from detectar_emociones import cargar_modelo_emociones
+from detectar_genero import cargar_modelo_genero
+from detectar_edad import cargar_modelo_edad
+
+modelo_emociones = None
+modelo_genero = None
+modelo_edad = None
+
+
+def cargar_modelos():
+    global modelo_emociones, modelo_genero, modelo_edad
+
+    if modelo_emociones is None:
+        print("Cargando modelo de emociones...")
+        modelo_emociones = cargar_modelo_emociones()
+
+    if modelo_genero is None:
+        print("Cargando modelo de género...")
+        modelo_genero = cargar_modelo_genero()
+
+    if modelo_edad is None:
+        print("Cargando modelo de edad...")
+        modelo_edad = cargar_modelo_edad()
+"""
